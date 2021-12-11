b4f5f136e9f85f16f5ef8e6efec8f11b45b81058
gxo <georgios.xonikis@apgsga.ch>
2021-12-11 12:32:55 +0100

    landing page and deployment to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from './shared/shared.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LandingPageComponent
     ],
     imports: [
         BrowserModule,
