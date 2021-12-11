f68511780fbe00446684cd5f82166e0005c45c3f
gxo <georgios.xonikis@gmail.com>
2021-12-11 11:27:57 +0100

    navbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KanbanBoardAngular12Firebase
+# Kanban Board with Angular 12 and Firebase
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.9.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-board-angular12-firebase';
+
+  constructor(private bo: BreakpointObserver) {
+
+    this.bo.observe(Breakpoints.Handset).subscribe(console.log);
+
+  }
+
+
 }
