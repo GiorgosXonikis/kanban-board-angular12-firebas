1d0f1a2a89d26810c53d0ec10cf69119b83d7197
gxo <georgios.xonikis@apgsga.ch>
2022-01-12 18:33:29 +0100

    improved snackbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 
 const routes: Routes = [
-    { path: '', component: LandingPageComponent },
+    { path: '', component: LandingPageComponent, canActivate: [AuthGuard] },
     {
         path: 'login',
         loadChildren: () => import('./user/user.module').then(m => m.UserModule)
--- src/app/services/snack-bar.service.ts
+++ src/app/services/snack-bar.service.ts
@@ -2,22 +2,32 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { tap } from 'rxjs/operators';
+import { MatSnackBarRef } from '@angular/material/snack-bar/snack-bar-ref';
+import { TextOnlySnackBar } from '@angular/material/snack-bar/simple-snack-bar';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SnackBarService {
-    constructor(private snackBar: MatSnackBar, private router: Router) {
+
+    constructor(private matSnackBar: MatSnackBar, private router: Router) {
     }
 
-    authError() {
-        this.snackBar
-            .open('You must be logged in!', 'OK', {
-                duration: 5000
+    warning(message = 'You must be logged in', action = 'OK', duration = 5000): MatSnackBarRef<TextOnlySnackBar> {
+        return this.matSnackBar.open(
+            message,
+            action, {
+                panelClass: ['snackbar-warning'],
+                duration
             })
-            .onAction()
-            .pipe(tap(_ => this.router.navigate(['/login'])))
-            .subscribe();
+    }
 
+    authError(): void {
+        this.warning()
+            .onAction()
+            .pipe(
+                tap(_ => this.router.navigate(['/login']))
+            )
+            .subscribe();
     }
 }
