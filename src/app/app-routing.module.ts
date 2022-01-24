cd452b722a39afe39d0f1a1f5d4381cf99b4a1b8
gxo <georgios.xonikis@apgsga.ch>
2022-01-24 11:10:09 +0100

    added readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 
 const routes: Routes = [
-    { path: '', component: LandingPageComponent },
+    { path: '', redirectTo: 'kanban', pathMatch: 'full' },
     {
         path: 'login',
         loadChildren: () => import('./user/user.module').then(m => m.UserModule)
